130983c5505bd875f9ac3148c7426eb2698a3888
nabeelah030 <80572116+nabeelah030@users.noreply.github.com>
2021-04-14 09:08:07 +0500

    first commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public int speed = 3;
+    public Rigidbody2D Rigidbody1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody1 = GetComponent < Rigidbody2D >();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mov = Input.GetAxis("Horizontal");
+        
+        Rigidbody1.velocity = new Vector2(mov * speed, Rigidbody1.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody1.velocity = new Vector2(Rigidbody1.velocity.y, speed);
+        }
+        
+    }
+}
